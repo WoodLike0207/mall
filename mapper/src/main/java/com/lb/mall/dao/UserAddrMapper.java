0abb5102f104b3685d7bd372d59f4448d6debf2e
WoodLike0207 <2416355572@qq.com>
2024-07-12 15:37:37 +0800

    查询用户收获地址接口实现

--- mapper/src/main/java/com/lb/mall/dao/UserAddrMapper.java
+++ mapper/src/main/java/com/lb/mall/dao/UserAddrMapper.java
@@ -2,6 +2,8 @@ package com.lb.mall.dao;
 
 import com.lb.mall.entity.UserAddr;
 import com.lb.mall.general.GeneralDAO;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserAddrMapper extends GeneralDAO<UserAddr> {
 }
